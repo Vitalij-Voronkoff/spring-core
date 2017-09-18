7d4a4ee1072f6669f3b0161f4ecc68be20c08936
Voronkov <VVoronkov@kiev.luxoft.com>
2017-09-18 18:32:49 +0300

    Simple spring core app.

---
+++ src/com/vvoronkov/spring_core/DrawingApp.java
@@ -0,0 +1,12 @@
+package com.vvoronkov.spring_core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle = context.getBean(Triangle.class, "triangle");
+        triangle.draw();
+    }
+}
